190b1201630c5e57953061a7b0777a43fd9cdf35
tom <tgs2315@gmail.com>
2013-07-10 23:57:14 -0400

    Again modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var file_buf = fs.readFileSync(index.html);
-var file_text = file_buf.toString("utf-8");
+var file_buf = fs.readFileSync('index.html');
+var file_text = file_buf.toString();
 
 app.get('/', function(request, response) {
   response.send(file_text);
